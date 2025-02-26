3b350048530ebaea0c614847239a0bb56eae97f3
Thomas Schmahl <thmschmahl@gmail.com>
2025-02-26 18:20:48 +0100

    feat: implement basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod cli;
+mod docker;
+use clap::Parser;
+use cli::{Cli, Command, ListCommands};
+
 fn main() {
-    println!("Hello, world!");
+    // Parse the cli input
+    let args = Cli::parse();
+
+    // handle the commands
+    match args {
+        Command::List { list_command } => match list_command {
+            ListCommands::Containers => {
+                println!("Listing all containers");
+            }
+        },
+    }
 }
