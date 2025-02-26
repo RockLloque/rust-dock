c8c36a2be73842a4ee2a18f7dfbf6f1cd28c8fba
Thomas Schmahl <thmschmahl@gmail.com>
2025-02-26 18:59:29 +0100

    feat: implement all flag

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,20 @@ mod docker;
 use clap::Parser;
 use cli::{Cli, Command, ListCommands};
 
+// #[tokio::main]
 fn main() {
     // Parse the cli input
     let args = Cli::parse();
 
     // handle the commands
-    match args {
+    match args.command {
         Command::List { list_command } => match list_command {
-            ListCommands::Containers => {
-                println!("Listing all containers");
+            ListCommands::Containers { all } => {
+                if all {
+                    println!("Listing all containers");
+                } else {
+                    println!("Listing only running containers");
+                }
             }
         },
     }
