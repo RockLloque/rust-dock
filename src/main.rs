5631fa23f67a0402ea46ae68a896b3009a3dd184
Thomas Schmahl <thmschmahl@gmail.com>
2025-02-27 02:00:01 +0100

    feat: implement list_containers

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,40 @@
 mod cli;
 mod docker;
+
+use bollard::secret::ContainerSummary;
 use clap::Parser;
 use cli::{Cli, Command, ListCommands};
+use docker::DockerClient;
 use dotenv::dotenv;
 
-// #[tokio::main]
-fn main() {
+#[tokio::main]
+async fn main() {
+    // Load the .env file
     dotenv().ok();
     // Parse the cli input
     let args = Cli::parse();
+    let docker_client = DockerClient::new(None);
 
     // handle the commands
     match args.command {
         Command::List { list_command } => match list_command {
             ListCommands::Containers { all } => {
-                if all {
-                    println!("Listing all containers");
-                } else {
-                    println!("Listing only running containers");
+                println!("Printing Containers");
+                match docker_client.list_containers(all).await {
+                    Ok(containers) => {
+                        for ContainerSummary {
+                            id, names, status, ..
+                        } in containers
+                        {
+                            println!(
+                                "{}\t{}\t{}",
+                                id.unwrap_or_default(),
+                                names.unwrap_or_default().join(","),
+                                status.unwrap_or_default()
+                            )
+                        }
+                    }
+                    Err(e) => eprintln!("Error listing containers: {}", e),
                 }
             }
         },
