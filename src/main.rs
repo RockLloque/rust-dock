c7ee84bfb9419eac0a3f1eeaf7a3c146561174d1
Thomas Schmahl <thmschmahl@gmail.com>
2025-02-27 01:40:45 +0100

    feat: install dotenv crate

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ mod cli;
 mod docker;
 use clap::Parser;
 use cli::{Cli, Command, ListCommands};
+use dotenv::dotenv;
 
 // #[tokio::main]
 fn main() {
+    dotenv().ok();
     // Parse the cli input
     let args = Cli::parse();
 
